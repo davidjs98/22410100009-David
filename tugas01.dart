53208cf50f16e528e95648e49762d53dfeca0f3c
David Julius <163961539+davidjs98@users.noreply.github.com>
2024-03-28 08:33:50 +0700

    Add files via upload

---
+++ tugas01.dart
@@ -0,0 +1,15 @@
+void main(){
+  var n = 75;
+  if(n>80 || n<100){
+    print('A');
+  } else if(n>65 || n<=80){
+    print('B');
+  } else if(n>50 || n<=65){
+    print('C');
+  } else if(n>35 || n<=50){
+    print('D');
+  }
+  else {
+    print('nilai sangat tidak manuk akal');
+  }
+}
